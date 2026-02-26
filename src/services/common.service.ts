5fcdb6f6dc02e04639c42ba2ca70a5f3bb49bfe0
Kunal Verma <vermakunal731@gmail.com>
2026-02-26 11:21:27 +0530

    Update image path

--- src/services/common.service.ts
+++ src/services/common.service.ts
@@ -1,20 +1,11 @@
-// export const getImagePath = (
-// 	defaultImage: string,
-// 	image?: string,
-// 	parentFolderPath: string = "components"
-// ): string => {
-// 	return image
-// 		? `/api/public${
-// 				process.env.NEXT_PUBLIC_DYNAMIC_IMAGES_BASE_PATH ?? ""
-// 		  }/${parentFolderPath}/${image?.trim()}`
-// 		: `${process.env.NEXT_PUBLIC_STATIC_IMAGES_BASE_PATH ?? ""}/${defaultImage}`;
-// };
 export const getImagePath = (
   defaultImage: string,
   image?: string,
   parentFolderPath: string = "components",
 ): string => {
   return image
-    ? `/assets/${parentFolderPath}/${image.trim()}`
-    : `/assets/img/${defaultImage}`;
+    ? `/api/public${
+        process.env.NEXT_PUBLIC_DYNAMIC_IMAGES_BASE_PATH ?? ""
+      }/${parentFolderPath}/${image?.trim()}`
+    : `${process.env.NEXT_PUBLIC_STATIC_IMAGES_BASE_PATH ?? ""}/${defaultImage}`;
 };
